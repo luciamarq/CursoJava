55e5f3e04c6b235f7ee590ce85b042ee23693f59
Lucia <lucia.marquez.garcia@accenture.com>
2023-01-18 10:59:04 +0100

    Nuevo Ejercicio AreaCirculo

--- Definiciones/src/operadores/AreaCirculo.java
+++ Definiciones/src/operadores/AreaCirculo.java
@@ -4,13 +4,13 @@ public class AreaCirculo {
 
 	public static void main(String[] args) {
 		int radio = 23;
-		final double PI = 3.1416;
+		final double NUM_PI = Math.PI;
 		
-		double area = (int)radio*radio*PI;
-		
-		
-		System.out.println(area);
-		
+		double area = (int)Math.pow(radio, 2)*NUM_PI;
+		double longitud = 2* NUM_PI*radio;
+	
+		System.out.println("El area de un circulo con radio: " + radio + " es " + area);
+		System.out.println("La longitud de un circulo con radio: " + radio + " es " + longitud);
 		
 		
 
